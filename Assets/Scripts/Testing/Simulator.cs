2c42c2f0e3238ed8e3950346dad4f8d49bd1275c
Raus <bloxxer14@gmail.com>
2018-09-27 20:08:18 -0400

    Fixed simulator script
    Properly handles the modified atmosphere class and shows gasses correctly

--- Assets/Scripts/Testing/Simulator.cs
+++ Assets/Scripts/Testing/Simulator.cs
@@ -8,7 +8,7 @@ using Atmos;
 public class Simulator : MonoBehaviour
 {
     public int width = 16;
-    public int height = 16;
+    public int length = 16;
     private Atmosphere atmos;
 
 	public float rate = 0f;
@@ -16,16 +16,28 @@ public class Simulator : MonoBehaviour
 	private float lastStep;
 	private int activeTiles = 0;
 
-	public bool debugDraw = false;
-	public float debugRadius = 3.5f;
+	public bool drawDebug = false;
+	public bool drawAll = false;
+	public float drawRadius = 3.5f;
 	public enum ViewType { Content, Pressure, Temperature, Combined };
-	public ViewType debugView = ViewType.Content;
+	public ViewType drawView = ViewType.Content;
+	
+	public Gasses gas = Gasses.Oxygen;
 	
 	void Start ()
     {
 		lastStep = Time.fixedTime;
 		
-		atmos = new Atmosphere(width, height);
+		atmos = new Atmosphere(width, length);
+
+		for (int i = 0; i < width; ++i)
+		{
+			for (int j = 0; j < length; ++j)
+			{
+				atmos.GetTile(i, j).MakeAir();
+				atmos.GetTile(i, j).RemoveFlux();
+			}
+		}
 	}
 
 	private void Thread ()
@@ -48,18 +60,30 @@ public class Simulator : MonoBehaviour
 			lastStep = Time.fixedTime + rate;
 		}
 
-		if (Input.GetKeyDown("z"))
+		Vector3 hit = GetMouse();
+
+		hit.x = Mathf.Round(hit.x);
+		hit.z = Mathf.Round(hit.z);
+
+		if (hit.x >= 0 && hit.x < width && hit.z >= 0 && hit.z < length)
 		{
-			for (int i = 0; i < width; ++i)
+			Tile tile = atmos.GetTile((int)hit.x, (int)hit.z);
+
+			if (Input.GetMouseButton(0))
 			{
-				for (int j = 0; j < height; ++j)
-				{
-					//atmos.GetTile(i, j).SetGasses(new float[] { (Mathf.Max(i, j) + 1f) * 15f, 0f, 0f, 0f });
-					atmos.GetTile(i, j).MakeAir();
-					atmos.GetTile(i, j).RemoveFlux();
-				}
+				tile.AddGas(gas, 60f);
+			}
+			else if (Input.GetMouseButton(1))
+			{
+				tile.SetGasses(new float[] { 0f, 0f, 0f, 0f });
+			}
+			else if (Input.GetMouseButton(2))
+			{
+				tile.Block();
 			}
 		}
+
+		//atmos.GetTile(i, j).SetGasses(new float[] { (Mathf.Max(i, j) + 1f) * 15f, 0f, 0f, 0f });
 		/*if (Input.GetKey("z"))
         {
             atmos[width - 1, 0].SetGases(new float[] { 0f, 0f, 0f, 0f });
@@ -118,25 +142,35 @@ public class Simulator : MonoBehaviour
 		}*/
 	}
 
+	private Vector3 GetMouse()
+	{
+		Plane plane = new Plane(Vector3.up, Vector3.zero);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float distance;
+
+		if (plane.Raycast(ray, out distance))
+		{
+			return ray.GetPoint(distance);
+		}
+
+		return Vector3.down;
+	}
+
 	private void OnDrawGizmos()
 	{
-		if (debugDraw)
+		if (drawDebug)
 		{
-			Plane plane = new Plane(Vector3.up, Vector3.up * 2);
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			float distance;
+			Vector3 hit = GetMouse();
 
-			if (plane.Raycast(ray, out distance))
+			if (hit != Vector3.down)
 			{
-				Vector3 hit = ray.GetPoint(distance);
-
 				for (int i = 0; i < width; ++i) //width-1; i > 0; --i)
 				{
-					for (int j = 0; j < height; ++j) //height-1; j > 0; --j)
+					for (int j = 0; j < length; ++j) //height-1; j > 0; --j)
 					{
 						Vector3 draw = new Vector3(i, 0, j) / 1f;
 
-						if (Vector3.Distance(draw, hit) < debugRadius || true)
+						if (Vector3.Distance(draw, hit) < drawRadius || drawAll)
 						{
 							Color state;
 							switch (atmos.GetTile(i, j).GetState())
@@ -147,46 +181,54 @@ public class Simulator : MonoBehaviour
 								default: state = new Color(0, 0, 0, 1); break;
 							}
 
-							switch (debugView)
+							float pressure;
+
+							if (atmos.GetTile(i, j).GetState() == TileStates.Blocked)
 							{
-								case ViewType.Content:
-									float[] gases = new float[5];
-									Color[] colors = new Color[] { Color.blue, Color.red, Color.gray, Color.magenta };
+								Gizmos.color = new Color(0.2f, 0.2f, 0.2f, 1f);
+								Gizmos.DrawCube(new Vector3(i, 0.5f, j), new Vector3(1, 1, 1));
+							}
+							else
+							{
+								switch (drawView)
+								{
+									case ViewType.Content:
+										float[] gases = new float[5];
+										Color[] colors = new Color[] { Color.blue, Color.red, Color.gray, Color.magenta };
 
-									float offset = 0f;
-									
-									for (int k = 0; k < 4; ++k)
-									{
-										float moles = atmos.GetTile(i, j).GetGasses()[k] / 30f;
+										float offset = 0f;
 
-										if (moles != 0f)
+										for (int k = 0; k < 4; ++k)
 										{
-											Gizmos.color = colors[k] - state;
-											Gizmos.DrawCube(new Vector3(i, moles / 2f + offset, j), new Vector3(1, moles, 1));
-											//Debug.DrawRay(new Vector3(i / 4f, offset, j / 4f), Vector3.up * grid[i, j].gases[k] / 30f, colors[k] - state);
-											offset += moles;
+											float moles = atmos.GetTile(i, j).GetGasses()[k] / 30f;
+
+											if (moles != 0f)
+											{
+												Gizmos.color = colors[k] - state;
+												Gizmos.DrawCube(new Vector3(i, moles / 2f + offset, j), new Vector3(1, moles, 1));
+												offset += moles;
+											}
 										}
-									}
-									//Debug.DrawRay(new Vector3(i, 0, j), new Vector3(grid[i, j].velocity.x, 0, grid[i, j].velocity.y), Color.red);
-									break;
-								/*case ViewType.Pressure:
-									Color color = Color.white - state;
+										break;
+									case ViewType.Pressure:
+										pressure = atmos.GetTile(i, j).GetPressure() / 30f;
 
-									Debug.DrawRay(new Vector3(i / 4f, 0, j / 4f), Vector3.up * atmos[i, j].GetPressure() / 30f, color);
-									break;
-								case ViewType.Temperature:
-									Color color2 = Color.red - state;
+										Gizmos.color = Color.white - state;
+										Gizmos.DrawCube(new Vector3(i, pressure / 2f, j), new Vector3(1, pressure, 1));
+										break;
+									case ViewType.Temperature:
+										float temperatue = atmos.GetTile(i, j).GetTemperature() / 100f;
 
-									Debug.DrawRay(new Vector3(i / 4f, 0, j / 4f), Vector3.up * atmos[i, j].temperature / 100f, color2);
-									break;
-								case ViewType.Combined:
-									float pressure = atmos[i, j].GetPressure() / 30f;
+										Gizmos.color = Color.red - state;
+										Gizmos.DrawCube(new Vector3(i, temperatue / 2f, j), new Vector3(1, temperatue, 1));
+										break;
+									case ViewType.Combined:
+										pressure = atmos.GetTile(i, j).GetPressure() / 30f;
 
-									Gizmos.color = new Color(atmos[i, j].temperature / 500f, 0, 0, 1) - state;
-									Gizmos.DrawCube(new Vector3(i, pressure / 2f, j), new Vector3(1, pressure, 1));
-
-									//Debug.DrawRay(new Vector3(i / 4f, 0, j / 4f), Vector3.up * grid[i, j].GetPressure() / 30f, temperature);
-									break;*/
+										Gizmos.color = new Color(atmos.GetTile(i, j).GetTemperature() / 500f, 0, 0, 1) - state;
+										Gizmos.DrawCube(new Vector3(i, pressure / 2f, j), new Vector3(1, pressure, 1));
+										break;
+								}
 							}
 						}
 					}
